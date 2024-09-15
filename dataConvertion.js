d92425d8fd5f43eba698e36f6f307c3d8a715cda
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-09-15 15:56:19 +0800

    Data Convertion

---
+++ dataConvertion.js
@@ -0,0 +1,41 @@
+// String to Number
+console.log("Convert String to Number");
+let price = "200";
+
+// parseInt
+let price1 = parseInt(price);
+console.log("parseInt:", price1);
+console.log(typeof price1);
+
+// Number
+let price2 = Number(price);
+console.log("\nNumber:", price2);
+console.log(typeof price2);
+
+// + operator
+let price3 = +price;
+console.log("\n+price:", price3);
+console.log(typeof price3);
+
+// Number to String
+console.log("\nConvert Number to String");
+let distance = 150;
+
+// .toString()
+let distance1 = distance.toString();
+console.log("distance.toString:", distance1);
+console.log(typeof distance1);
+
+// String
+let distance2 = String(distance);
+console.log("\nString:", distance2);
+console.log(typeof distance2);
+
+// String to Decimal Number
+console.log("\nConvert String to Decimal Number");
+let weight = "72.5";
+
+// parseFloat
+let weightFloat = parseFloat(weight);
+console.log("parseFloat:", weightFloat);
+console.log(typeof weightFloat);
